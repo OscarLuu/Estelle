0ae514d9bda3b2f46acb3e172eff96718d96dbe5
Oscar Luu <oscarluu26@gmail.com>
2019-02-11 19:51:47 -0800

    Final touches

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { FaLinkedinIn, FaAt } from "react-icons/fa";
 
 const Contact = () => {
-  return <p>contact</p>;
+  return (
+    <div>
+      <div className="home">
+        <div className="text">
+          <p className="name fade-in one">CONTACT ME</p>
+        </div>
+        <a href="https://www.linkedin.com/in/estelle-johnson/">
+          <button className="contact-button first fade-in three">
+            <FaLinkedinIn className="icon" />
+          </button>
+        </a>
+
+        <a href="mailto:estellemarjohnson@gmail.com">
+          <button className="contact-button fade-in four">
+            <FaAt className="icon" />{" "}
+          </button>
+        </a>
+      </div>
+      <img
+        src={require("../svg/bayarea.png")}
+        className="bay-area fade-in three"
+      />
+    </div>
+  );
 };
 
 export default Contact;
